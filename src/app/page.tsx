220203b9c5493929adb1b1a6268784c00a565463
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2025-07-17 15:59:58 -0500

    chore: setup react query | redux | cookeie | axios client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Box>Hello World!</Box>
-      <Button> Click me</Button>
+      <Button>Click me</Button>
       <Box as="section" onClick={() => console.log('hello world')}>
         Hello world{' '}
       </Box>
--- src/lib/providers/store.providers.tsx
+++ src/lib/providers/store.providers.tsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { PersistGate } from 'redux-persist/integration/react';
-import store, { persistor } from './store';
 import { Provider } from 'react-redux';
+import { persistor, store } from '@/redux';
 
-export const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
+export const AppStoreProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
