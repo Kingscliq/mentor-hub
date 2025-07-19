9c9ef486d464fb9cca888890fb7c62c9e15637fe
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2025-07-18 23:39:12 -0500

    chore: add logger class for logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 'use client';
 import Box from '@/components/ui/box';
 import { Button } from '@/components/ui/button';
+import { Logger } from '@/lib';
+import { toast } from 'sonner';
 
 export default function Home() {
   return (
     <>
       <Box>Hello World!</Box>
       <Button>Click me</Button>
-      <Box as="section" onClick={() => console.log('hello world')}>
+      <Button onClick={() => toast.success('Hello this is a success message')}>
+        Click me
+      </Button>
+      <Box
+        as="section"
+        onClick={() => Logger.log('Hello this is an information')}
+      >
         Hello world{' '}
       </Box>
     </>
