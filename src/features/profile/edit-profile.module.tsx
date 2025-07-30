aa4c3de3e1bbc6aecdba606f2ad3f06d52477dce
nuella001 <chiomamemmanuella@gmail.com>
2025-07-30 02:42:57 +0100

    feat: Add Edit profile

---
+++ src/features/profile/edit-profile.module.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import EditProfileForm from '@/features/profile/components/edit-profile-form';
+
+export default function EditProfilePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <EditProfileForm />
+    </div>
+  );
+}
